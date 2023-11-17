ad853936d431c74e56255edaf28b9943044ac496
capt-red <varun.dunna2524@gmail.com>
2023-11-17 14:03:43 +0530

    Commit files

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,39 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 
+const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem("userState");
+    if (serializedState === null) {
+      return undefined;
+    }
+    return {
+      user: JSON.parse(serializedState),
+    };
+  } catch (err) {
+    return undefined;
+  }
+};
+
+const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state.user);
+    localStorage.setItem("userState", serializedState);
+  } catch (err) {
+    // Handle errors here
+  }
+};
+
 const store = configureStore({
   reducer: {
     user: userReducer,
     // Add other reducers as needed
   },
+  preloadedState: loadState(),
+});
+
+store.subscribe(() => {
+  saveState(store.getState());
 });
 
 export default store;
